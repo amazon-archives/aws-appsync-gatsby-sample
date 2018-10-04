feddd4c98d9e463b47e8e6a1159cb5928bc7f452
Rohan Deshpande <deshpan@amazon.com>
2018-10-04 13:48:20 -0700

    Added screenshot

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import EventList from "../components/EventList"
 
 const Index = ({ data }) => (
     <div>
-        <h1>Events</h1>
+        <h1>AWS AppSync Events</h1>
+        <p><a href="https://aws.amazon.com/appsync/">AWS AppSync</a> is a serverless backend for web and mobile apps powered by GraphQL that provides real-time and offline data access. <a href="https://github.com/aws-samples/aws-appsync-gatsby-sample">This sample</a> renders a list of events retrieved from an AWS AppSync GraphQL API into a Gatsby site.<hr /></p>
         <EventList events={data.events.listEvents.items} />
     </div>
 )
